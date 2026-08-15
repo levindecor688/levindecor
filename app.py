e9e7c68029060c7ded30d2b525fcd5e6d742f3c5
levindecor688 <levindecor@yahoo.com>
2026-08-15 18:53:37 +0530

    Add legacy-cgi support and python version definition

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+import sys
+try:
+    import cgi
+except ImportError:
+    try:
+        import legacy_cgi as cgi
+        sys.modules['cgi'] = cgi
+    except ImportError:
+        pass
+
 from mylevin.wsgi import application as app
 
 if __name__ == '__main__':
     from django.core.management import execute_from_command_line
     import os
-    import sys
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mylevin.settings')
     execute_from_command_line(sys.argv)
